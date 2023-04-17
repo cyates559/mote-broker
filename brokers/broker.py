a5c779be26cb6cc7864089331a90b38bc2126981
Carsen Yates <carsenyates559@gmail.com>
2023-04-16 18:45:17 -0700

    pycharm

--- brokers/broker.py
+++ brokers/broker.py
@@ -122,6 +122,7 @@ class Broker:
             if message.tree:
                 if message.update:
                     rows = message.flatten_into_rows(self.tree)
+                    print("THE ROWS", rows)
                 else:
                     # create objects
                     rows = NotImplemented
