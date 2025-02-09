cf1dc16a0e10d6d21adb7dee40f628e5e27dca89
Carsen Yates <carsenyates559@gmail.com>
2025-02-08 20:19:52 -0800

    test-fix

--- servers/ssl.py
+++ servers/ssl.py
@@ -12,7 +12,26 @@ class SecureSocketServer(SocketServer):
     ssl_context: SSLContext = None
 
     @cached_property
+    def base_socket(self):
+        return super().server
+
+    @cached_property
     def server(self):
         if self.ssl_context:
-            return self.ssl_context.wrap_socket(socket(AF_INET, SOCK_STREAM))
-        return socket(AF_INET, SOCK_STREAM)
+            return self.ssl_context.wrap_socket(self.base_socket)
+        return self.base_socket
+
+
+    def stop(self):
+        # self.alive = False
+        # while self.clients:
+        #     self.clients.pop().disconnect()
+        # stop_socket(self.host, self.port)
+        # try:
+        #     self.server.shutdown(SHUT_RDWR)
+        # except OSError as e:
+        #     if e.errno not in [9, 57, 107]:
+        #         raise
+        # self.server.close()
+        super().stop()
+        self.base_socket.close()
